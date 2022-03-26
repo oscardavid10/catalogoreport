9206d257a4018c3479152ae89d3a1a8c144a2862
oscardavid10 <david_010@live.com.mx>
2022-03-26 14:15:48 -0600

    Update CSS

--- index.php
+++ index.php
@@ -13,7 +13,28 @@
 <body>
     <div class="container-full">
         <div id="header">
-            <img src="image/logo.png" alt="Logo de la pagina" width="120px" height="120px">
+            <img src="image/logo.png" class="logo" alt="Logo de la pagina" width="120px" height="120px">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Navbar</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Features</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Pricing</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </div>
     </div>
     <footer class="footer">
