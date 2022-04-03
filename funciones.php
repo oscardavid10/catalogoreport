434f93e7fa27b835b04825d8ba98683c61c5e452
oscardavid10 <david_010@live.com.mx>
2022-04-03 17:28:30 -0500

    Update funciones.php

--- funciones.php
+++ funciones.php
@@ -2,7 +2,7 @@
 
 function ConexionBasedeDatos(){
     try{
-        $conexion = new mysqli('us-cdbr-east-05.cleardb.net','b3318d6e4070df','c5137977','usuarios');
+        $conexion = new mysqli('us-cdbr-east-05.cleardb.net','b3318d6e4070df','c5137977','heroku_efd1ce7f7ebb766');
     }catch(PDOException $e){
         echo "Error: " . $e->getMessage();
     }
