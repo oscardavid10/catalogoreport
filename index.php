85f7c3053b25fb8c2bf89d165e8fefa551232fe3
oscardavid10 <david_010@live.com.mx>
2022-04-03 21:26:24 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- <link href="css/bootstrap.min.css" rel="stylesheet"> -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link href="css/adicional.css" rel="stylesheet">
     <link href="css/jquery.dataTables.min.css" rel="stylesheet"></link>
 
