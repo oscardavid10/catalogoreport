b6b1d7916dde94c4aed33f83d415466c6db3ab30
oscardavid10 <david_010@live.com.mx>
2022-04-03 21:54:48 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,22 +6,22 @@
     <link href="css/adicional.css" rel="stylesheet">
     <link href="css/jquery.dataTables.min.css" rel="stylesheet"></link>
 
-    <script src="js/jquery-3.3.1.js"></script>
+    <script src="js/jquery-3.5.1.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/jspdf.debug.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/autoNumeric.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap-notify.js"></script>
+  <script src="js/SimpleAjaxUploader.js"></script>
+  <script src="js/jquery.dataTables.min.js"></script>
+
+    <!-- <script src="js/jquery-3.3.1.js"></script>
     <script src="js/bootstrap.bundle.min.js"></script>
-    <script src="js/jquery.dataTables.min.js"></script>
+    <script src="js/jquery.dataTables.min.js"></script> -->
     <script type="text/javascript" src="js/sweetalert/package/dist/sweetalert2.all.js"></script>
 
-    <!-- <script src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.flash.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.bootstrap4.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.print.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.colVis.min.js"></script> -->
-
-    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
 
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -234,8 +234,6 @@
 </div>
 
 
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 <script type="text/javascript">
 
 
