1c7dd9c400050c720c53c6acef00d76b49f98f89
oscardavid10 <david_010@live.com.mx>
2022-05-02 23:15:38 -0500

    make it better

--- index.php
+++ index.php
@@ -44,13 +44,6 @@
                                 <li class="nav-item">
                                     <a class="nav-link active" aria-current="page" href="#">Inicio</a>
                                 </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Features</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Pricing</a>
-                                </li>
-                            </ul>
                         </div>
                     </div>
                 </nav>
